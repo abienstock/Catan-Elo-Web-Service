50e301f945f2407c50d735aa065b65d0632a940b
Alex Bienstock <afb2136@columbia.edu>
2018-05-27 16:08:43 -0400

    fixed regex for league, uname urls.. specified for new leagues

--- catansite/urls.py
+++ catansite/urls.py
@@ -20,18 +20,18 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.landing, name='landing'),
-	url(r'^(?P<league_name>[a-zA-z]+)/leaderboard/$', views.leaderboard, name='leaderboard'),
-    url(r'^(?P<league_name>[a-zA-z]+)/results/(?P<game_id>[0-9]+)/$', views.game_results, name='game_results'),
+	url(r'^(?P<league_name>[a-zA-z0-9]+)/leaderboard/$', views.leaderboard, name='leaderboard'),
+    url(r'^(?P<league_name>[a-zA-z0-9]+)/results/(?P<game_id>[0-9]+)/$', views.game_results, name='game_results'),
     url(r'^new_game/$', views.new_game, name='new_game'),    
-    url(r'^(?P<league_name>[a-zA-z]+)/new_game/$', views.new_game, name='new_game'),    
-    url(r'^(?P<league_name>[a-zA-z]+)/games/$', views.games, name='games'),
-    url(r'^(?P<league_name>[a-zA-z]+)/games/(?P<game_id>[0-9]+)/$', views.games, name='games'),    
-    url(r'^(?P<league_name>[a-zA-z]+)/player_stats/$', views.player_stats_main, name='player_stats_main'),    
-    url(r'^(?P<league_name>[a-zA-z]+)/player_stats/(?P<username>[a-zA-Z]+)/$', views.player_stats, name='player_stats'),
+    url(r'^(?P<league_name>[a-zA-z0-9]+)/new_game/$', views.new_game, name='new_game'),    
+    url(r'^(?P<league_name>[a-zA-z0-9]+)/games/$', views.games, name='games'),
+    url(r'^(?P<league_name>[a-zA-z0-9]+)/games/(?P<game_id>[0-9]+)/$', views.games, name='games'),    
+    url(r'^(?P<league_name>[a-zA-z0-9]+)/player_stats/$', views.player_stats_main, name='player_stats_main'),    
+    url(r'^(?P<league_name>[a-zA-z0-9]+)/player_stats/(?P<username>[a-zA-Z0-9@.+-_]+)/$', views.player_stats, name='player_stats'),
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/new_account/$', views.new_acct, name='new_acct'),
     url('accounts/', include('django.contrib.auth.urls')),
     url(r'^new_league/$', views.new_league, name='new_league'),
     url(r'^leagues/$', views.leagues, name='leagues'),
-    url(r'^(?P<league_name>[a-zA-z]+)/$', views.league_home, name='league_home')
+    url(r'^(?P<league_name>[a-zA-z0-9]+)/$', views.league_home, name='league_home')
 ]
